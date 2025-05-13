8085b7958d793cb59a5705eecab43af58c95a496
happiness <happinesskhalan@gmail.com>
2025-05-13 23:22:24 +0300

    object inheritance lab complete

--- lib/student.py
+++ lib/student.py
@@ -2,7 +2,14 @@
 
 from user import User
 
+
 class Student(User):
+    knowledge = list()
+
     
-    def learn(self):
+    def learn(self, subject):
+        return self.knowledge.append(subject)
         pass
+Amelia = Student("Amelis", "Maina")
+print(Amelia.first_name)
+print(Amelia.last_name)
